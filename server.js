8b115477f8619612a6f840e730f1b8a478c60474
Donny <donaldk@shaw.ca>
2016-12-01 03:29:20 +0000

    revised listening port

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ var server = http.createServer(function (request, response) {
   
 })
 
-server.listen(8080, function(req, res) {
+server.listen(process.env.PORT || 8080, function(req, res) {
   console.log('Timestamp microservice')
 })
